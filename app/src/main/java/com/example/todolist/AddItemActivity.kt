c5f0c9a15ad7726a38796d3fa5085b4630967701
DianaTarnu <diana.tarnu@gmail.com>
2020-10-22 22:15:00 +0300

    Make activity connectiins

--- app/src/main/java/com/example/todolist/AddItemActivity.kt
+++ app/src/main/java/com/example/todolist/AddItemActivity.kt
@@ -1,11 +1,30 @@
 package com.example.todolist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class AddItemActivity : AppCompatActivity() {
+    private lateinit var itemNameEditTextView: TextView
+    private lateinit var priorityCheckBox: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_item)
+
+        itemNameEditTextView = findViewById(R.id.enter_item)
+        priorityCheckBox = findViewById(R.id.priority_checkbox)
+    }
+
+    public fun saveItemAction(view: View){
+
+    }
+
+    public fun cancelAction(view: View){
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoItemRecyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        todoItemRecyclerView = findViewById(R.id.todo_item_recycler_view)
+
+    }
+
+    //switch between the pages with onclick listener
+    public fun navToAddItemAction(view: View){
+        val intent: Intent = Intent(this, AddItemActivity::class.java)
+        startActivity(intent)
     }
 }
