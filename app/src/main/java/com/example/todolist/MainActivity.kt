09a1c2cb504218531f5cca47e21c3d6d486faa2a
DianaTarnu <diana.tarnu@gmail.com>
2020-10-23 12:27:17 +0300

    Add iten views and adapters

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -4,21 +4,39 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var todoItemRecyclerView: RecyclerView
+    private lateinit var recyclerAdapter: ToDoItemsAdapter
+    private lateinit var recyclerLayoutManager: RecyclerView.LayoutManager
+
+    var todoItemsList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        todoItemRecyclerView = findViewById(R.id.todo_item_recycler_view)
+        todoItemsList.add("Buy groceries")
+        todoItemsList.add("Do laundry")
+        todoItemsList.add("Play guitar")
 
+        todoItemRecyclerView = findViewById(R.id.todo_item_recycler_view)
+        recyclerLayoutManager = LinearLayoutManager(this)
+        recyclerAdapter = ToDoItemsAdapter(todoItemsList)
+
+        todoItemRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = recyclerLayoutManager
+            adapter = recyclerAdapter
+        }
     }
 
     //switch between the pages with onclick listener
     public fun navToAddItemAction(view: View){
+        // An intent is an abstract description of an operation to be performed.
+        // Its most significant use is in the launching of activities, where it can be thought of as the glue between activities
         val intent: Intent = Intent(this, AddItemActivity::class.java)
         startActivity(intent)
     }
---
+++ app/src/main/java/com/example/todolist/ToDoItemsAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class ToDoItemsAdapter(private val todoItemsList:ArrayList<String>):
+    RecyclerView.Adapter<ToDoItemsAdapter.ViewHolder>() {
+
+    // val e RootLayout din to_do_item_layout
+    class ViewHolder (val constraintLayout: ConstraintLayout):RecyclerView.ViewHolder(constraintLayout)
+
+    //Methods we need
+    //called when we create a new instance of a ViewHolder; provide extra set ups as soon as the view is created(appearance, functionality)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //we can access each item in the to_do_item_layout with getChild method
+        val constraintLayout = LayoutInflater.from(parent.context).inflate(
+            R.layout.to_do_item_layout, parent, false) as ConstraintLayout
+
+        return ViewHolder(constraintLayout)
+    }
+
+    //bind the data we find in the todoItemsList to each of the views, so that we create a custom view for each of the items
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val constraintLayout = holder.constraintLayout  //holder. the val we passed into the class ViewHolder
+        val nameTextView = constraintLayout.getChildAt(0) as TextView
+        nameTextView.text = todoItemsList[position]
+    }
+
+    //return how many items we have in our list
+    override fun getItemCount(): Int {
+        return todoItemsList.size
+    }
+}
